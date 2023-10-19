33f92a5c9db62d8b8ca77e1de1a7e9ee62c57351
Pyae Sone Nyein <pyaesonenyein1996@gmail.com>
2023-10-19 11:32:38 +0630

    add:commands

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "hello darkness my old friend";
 });
+
+
+Route::get('/clear', function (){
+    Artisan::call('cache:clear');
+});
+Route::get('/config', function (){
+    Artisan::call('config:clear');
+});
+Route::get('/route', function (){
+    Artisan::call('route:clear');
+});
+
+Route::get('/db1wipe', function (){
+    Artisan::call('db:wipe --database=mysql');
+});
+Route::get('/db2wipe', function (){
+    Artisan::call('db:wipe --database=mysql_2');
+});
+
+Route::get('/migrate', function (){
+    Artisan::call('migrate ');
+});
+
+
