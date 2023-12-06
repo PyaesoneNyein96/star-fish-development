2e52c9816d1663d996bbd56320014dd546f15351
Htoo Antt Ko <kohtooantt@gmail.com>
2023-12-06 16:50:24 +0630

    seed all grade 1

---
+++ database/seeders/AnsNQueGrade3Seeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AnsNQueGrade3Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        ////////////////////////////////////////////////////////
+        ////    Grade 3  Lesson 26 Game 104 (  Writing ( Topic )  )
+        ////////////////////////////////////////////////////////
+
+    }
+}
