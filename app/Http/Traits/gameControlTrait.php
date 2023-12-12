3f3e0d3b13933e78daf31251f8dde3e1a81a29ce
Pyae Sone Nyein <pyaesonenyein1996@gmail.com>
2023-12-12 17:01:34 +0630

    game status control process 2

--- app/Http/Traits/gameControlTrait.php
+++ app/Http/Traits/gameControlTrait.php
@@ -10,13 +10,14 @@ trait gameControlTrait
 
     public function lockAll(Request $request){
 
+        $games = explode(",", $request->lock_games);
+
         if($request->lock_games !== null ){
-            $lockGames = Game::whereIn('id', $request->lock_games)->get();
+            Game::whereIn('id', $games)->update(['status' => 0]);
 
-            return $lockGames;
-            $lockGames->update('status',0);
+            return Game::whereIn('id', $games)->get();
 
-            // if($lockGames) return "$request->lock_games were locked";
+
         }
 
 
