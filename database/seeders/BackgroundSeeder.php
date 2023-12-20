7222638e68737705f2a08e25d58efe09211970bc
Pyae Sone Nyein <pyaesonenyein1996@gmail.com>
2023-12-20 14:42:02 +0630

    lesson_31_U_3

--- database/seeders/BackgroundSeeder.php
+++ database/seeders/BackgroundSeeder.php
@@ -19,40 +19,6 @@ class BackgroundSeeder extends Seeder
         $ImageDomain = $domain."/storage/images/";
 
 
-        // G-1,L-1,U-4
-
-        DB::table('background_images')->insert([
-            ['title' => "someTitle", 'round_id' => 1,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg1".".png",],
-            ['title' => "someTitle", 'round_id' => 2,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg1".".png",],
-            ['title' => "someTitle", 'round_id' => 3,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg1".".png",],
-            ['title' => "someTitle", 'round_id' => 4,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg1".".png",],
-            ['title' => "someTitle", 'round_id' => 8,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg1".".png",],
-            ['title' => "someTitle", 'round_id' => 9,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg1".".png",],
-            ['title' => "someTitle", 'round_id' => 10,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg1".".png",],
-                        // ============== //
-             ['title' => "someTitle", 'round_id' => 5,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg2".".png",],
-            ['title' => "someTitle", 'round_id' => 6,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg2".".png",],
-            ['title' => "someTitle", 'round_id' => 7,
-            'image' => $ImageDomain."/Backgrounds/"."G1_L1_U4_bg2".".png",],
-        ]);
-
-
-        //G-1,L-17,U-2
-
-        // DB::table('background_images')->insert([
-        //      ['title' => "someTitle", 'game_id' => 36,
-        //     'image' => "G1_L7_U3_background".".png",],
-        // ]);
-
 
 
     }
