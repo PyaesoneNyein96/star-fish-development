45d42f9c0229711184f531bda17090c21380eff1
Htoo Antt Ko <kohtooantt@gmail.com>
2024-01-02 17:00:47 +0630

    fix

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -141,7 +141,7 @@ class GameSeeder extends Seeder
 
             ['name' => 'Target Language & Grammar Explanation', 'unit_id' => 59,  'category_id' => 1, 'instructionGIF' => null],
             ['name' => 'Read, Listen and Repeat', 'unit_id' => 60,  'category_id' => 2, 'instructionGIF' => null],
-            ['name' => 'Listen and Practice', 'unit_id' => 61,  'category_id' => 6, 'instructionGIF' => null],
+            ['name' => 'Listen and Practice', 'unit_id' => 61,  'category_id' => 26, 'instructionGIF' => null],
             ['name' => 'Matching', 'unit_id' => 62,  'category_id' => 7, 'instructionGIF' => $Gif_path . "Matching.gif"],
             ['name' => 'Speaking Practice', 'unit_id' => 63,  'category_id' => 11, 'instructionGIF' => $Gif_path . "Speaking.gif"],
             ['name' => 'Reading Passage', 'unit_id' => 64,  'category_id' => 16, 'instructionGIF' => null],
@@ -524,6 +524,7 @@ class GameSeeder extends Seeder
         ]);
 
         //  Grade 1 Games end.......
+
 
         // //----------------- Grade 2 games -------------------------
 
