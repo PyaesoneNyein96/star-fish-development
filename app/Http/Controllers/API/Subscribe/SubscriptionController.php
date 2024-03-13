10ecb448abd5f42e6bf8102c2fa059cdbd839885
Htoo Antt Ko <kohtooantt@gmail.com>
2024-03-13 10:52:36 +0630

    test subs

--- app/Http/Controllers/API/Subscribe/SubscriptionController.php
+++ app/Http/Controllers/API/Subscribe/SubscriptionController.php
@@ -15,6 +15,7 @@ use App\Models\StudentLesson;
 use App\Models\SubscriptionPlan;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\StudentUnit;
 use Illuminate\Support\Facades\Http;
 
 class SubscriptionController extends Controller
@@ -23,33 +24,33 @@ class SubscriptionController extends Controller
 
     public $token, $grade_id, $subscription_id, $student;
 
-    public function __construct(Request $request) {
+    public function __construct(Request $request)
+    {
         $this->token = $request->header('token');
         $this->grade_id = $request->header('grade_id');
         $this->subscription_id = $request->header('subscription_id');
 
-        $this->student = Student::where('token',$request->header('token'))->first();
-
-
+        $this->student = Student::where('token', $request->header('token'))->first();
     }
 
     ////////////////////////////////////////////////////////////////
 
-    public function plans(Request $request){
+    public function plans(Request $request)
+    {
 
         $student = Student::where('token', $this->token)->first();
         return Subscription::all();
-
     }
 
 
-    public function purchase(Request $request){
+    public function purchase(Request $request)
+    {
 
 
 
-        $student = Student::where('token', $this->token)->where('status',1)->first();
+        $student = Student::where('token', $this->token)->where('status', 1)->first();
 
-        if(!$student){
+        if (!$student) {
             return response()->json([
                 "status" => "you are not allowed for this process."
             ], 403);
@@ -62,7 +63,7 @@ class SubscriptionController extends Controller
 
             return $purchasing = $this->purchasing($student, $this->grade_id, $this->subscription_id);
 
-            if($purchasing == 1){
+            if ($purchasing == 1) {
 
                 $now = Carbon::now(strval($student->country['timezone']));
 
@@ -81,8 +82,8 @@ class SubscriptionController extends Controller
                     'updated_at' => Carbon::now(strval($student->country['timezone']))
                 ]);
 
-                $latest_date = StudentGrade::where('grade_id',$this->grade_id)
-                ->pluck('expire_date');
+                $latest_date = StudentGrade::where('grade_id', $this->grade_id)
+                    ->pluck('expire_date');
 
                 $this->addedSubscriptionDate($latest_date);
 
@@ -92,130 +93,122 @@ class SubscriptionController extends Controller
                 return response()->json([
                     'status' => "successfully purchased.",
                 ], 200);
-
             }
-
-
         } catch (\Throwable $th) {
             DB::rollback();
             return $th;
-
         }
 
         return "already purchased";
-
     }
 
 
-    public function removePlan(Request $request) {
+    public function removePlan(Request $request)
+    {
 
-            $student = Student::where('token', $this->token)->where('status',1)->first();
+        $student = Student::where('token', $this->token)->where('status', 1)->first();
 
-            $gradeLessons = Grade::find($this->grade_id)->lessons;
+        $gradeLessons = Grade::find($this->grade_id)->lessons;
 
-            $gradeUnit = Grade::find($this->grade_id)->units->pluck('id');
+        $gradeUnit = Grade::find($this->grade_id)->units->pluck('id');
 
-            $gradeGames = Game::whereIn('unit_id', $gradeUnit)->get();
-            // return $gradeGames;
+        $gradeGames = Game::whereIn('unit_id', $gradeUnit)->get();
+        // return $gradeGames;
 
-            DB::beginTransaction();
+        DB::beginTransaction();
 
-            try {
+        try {
 
-                $del_lessons = $gradeLessons->filter(function($l) use($student) {
-                    return $student->lessons->contains('id', $l->id);
-                })->pluck('id');
+            $del_lessons = $gradeLessons->filter(function ($l) use ($student) {
+                return $student->lessons->contains('id', $l->id);
+            })->pluck('id');
 
-                $res1= StudentLesson::where('student_id', $student->id)->whereIn('id', $del_lessons)->delete();
-
-            //////////////////////////////
-
-                $del_units = $gradeUnit->filter(function($l) use($student){
-                    return $student->units->contains('id', $l->id);
-                })->pluck('id');
-
-                $res2 = StudentUnit::where('student_id', $student->id)->whereIn('id', $del_units)->delete();
+            $res1 = StudentLesson::where('student_id', $student->id)->whereIn('id', $del_lessons)->delete();
 
             //////////////////////////////
 
-                $del_games = $gradeGames->filter(function($g) use($student) {
-                    return $student->games->contains('id', $g->id);
-                })->pluck('id');
+            $del_units = $gradeUnit->filter(function ($l) use ($student) {
+                return $student->units->contains('id', $l->id);
+            })->pluck('id');
 
-                $res3 = StudentGame::where('student_id',$student->id)->whereIn('id',$del_games)->delete();
+            $res2 = StudentUnit::where('student_id', $student->id)->whereIn('id', $del_units)->delete();
 
-                if($res1 && $res3){
-                    StudentGrade::where('grade_id',$this->grade_id)->delete();
-                }
+            //////////////////////////////
 
-                DB::commit();
+            $del_games = $gradeGames->filter(function ($g) use ($student) {
+                return $student->games->contains('id', $g->id);
+            })->pluck('id');
 
-                return true;
+            $res3 = StudentGame::where('student_id', $student->id)->whereIn('id', $del_games)->delete();
 
+            if ($res1 && $res3) {
+                StudentGrade::where('grade_id', $this->grade_id)->delete();
+            }
 
-           } catch (\Throwable $th) {
-                DB::rollback();
-                return $th;
-           }
+            DB::commit();
 
-
-
-
+            return true;
+        } catch (\Throwable $th) {
+            DB::rollback();
+            return $th;
+        }
     }
 
 
-    private function purchasing($student, $grade_id, $subscription_id) {
+    private function purchasing($student, $grade_id, $subscription_id)
+    {
 
         $kbzRequestURL = "http://api.kbzpay.com/payment/gateway/uat/precreate";
 
-        $order_id = rand(1,999).chr(rand(65, 90));
-        $time = Carbon::now();
+        // $order_id = rand(1, 999) . chr(rand(65, 90));
+        $order_id = rand(1, 999);
+        $time = Carbon::now()->timestamp;
+
         $data = [
             "Request" => [
-                "timestamp" => $time,
-                "method" => "kbz.payment.precreate",
+                "timestamp" => (string)$time,
                 "notify_url" => "https://star-fish-development.myanmargateway.net/payment/notify",
                 "nonce_str" => "8264ILTKCH16CQ2502SI8ZNMTM67VS_3",
                 "sign_type" => "SHA256",
+                "method" => "kbz.payment.precreate",
                 "sign" => strtoupper($this->convert_SHA256($order_id, $time)),
                 "version" => "1.0",
                 "biz_content" => [
-                    "merch_order_id" => "$order_id",
+                    // "merch_order_id" => "$order_id",
+                    "merch_order_id" => str_pad($order_id, 9, '0', STR_PAD_LEFT),
                     "merch_code" => "70050901",
                     "appid" => "kp0480c579f02f48ae8c37ce82260511",
-                    "trade_type" => "APP",
-                    "total_amount" => "1000",
+                    "trade_type" => "PWAAPP",
+                    "total_amount" => "20000",
                     "trans_currency" => "MMK",
-
                 ]
             ]
         ];
 
-
         // return $data;
 
 
-        $responseFromKBZServer = Http::post($kbzRequestURL,json_encode($data, JSON_PRETTY_PRINT));
+        // $responseFromKBZServer = Http::post($kbzRequestURL, json_encode($data, JSON_PRETTY_PRINT));
+        $responseFromKBZServer = Http::post($kbzRequestURL, json_encode($data, JSON_PRETTY_PRINT));
 
         return $responseFromKBZServer;
-
     }
 
 
 
-    private function convert_SHA256($order_id, $time){
+    private function convert_SHA256($order_id, $time)
+    {
 
         // $stringA = "appid=kp123456789987654321abcdefghijkl&merch_code=100001&merch_order_id=201811212009001&method=kbz.payment.precreate&nonce_str=845255910308564481&notify_url=http://test.com/payment/notify&timestamp=1536637503&total_amount=1000&trade_type=APPH5&trans_currency=MMK&version=1.0";
 
-        $raw = "appid=kp0480c579f02f48ae8c37ce82260511"."&merch_code=70050901". "&merch_order_id=".$order_id."&method=kbz.payment.precreate"."&nonce_str=8264ILTKCH16CQ2502SI8ZNMTM67VS_3"."&notify_url=https://star-fish-development.myanmargateway.net/payment/notify"."&timestamp=".$time."&total_amount=1000&trade_type=APPH5&trans_currency=MMK&version=1.0";
+        $raw = "appid=kp0480c579f02f48ae8c37ce82260511" . "&merch_code=70050901" . "&merch_order_id=" . $order_id . "&method=kbz.payment.precreate" . "&nonce_str=8264ILTKCH16CQ2502SI8ZNMTM67VS_3" . "&notify_url=https://star-fish-development.myanmargateway.net/payment/notify" . "&timestamp=" . $time . "&total_amount=1000&trade_type=APPH5&trans_currency=MMK&version=1.0";
 
-        $combine = $raw."&key=starfish@123";
+        $combine = $raw . "&key=starfish@123";
 
         $hashed = hash('sha256', $combine);
 
 
         return $hashed;
-
     }
 
 
@@ -225,21 +218,15 @@ class SubscriptionController extends Controller
     /////////////////////////////////////////////////////
 
 
-    private function addedSubscriptionDate($latest_date){
+    private function addedSubscriptionDate($latest_date)
+    {
 
         $grades = StudentGrade::where('student_id', $this->student->id)->get();
 
-        $grades->filter(function($g) use($latest_date){
-            if($g->expire_date !== $latest_date){
-               $g->update(['expire_date' => Carbon::now()->addYear()]);
+        $grades->filter(function ($g) use ($latest_date) {
+            if ($g->expire_date !== $latest_date) {
+                $g->update(['expire_date' => Carbon::now()->addYear()]);
             }
         });
-
     }
-
-
-
-
-
-
 }
