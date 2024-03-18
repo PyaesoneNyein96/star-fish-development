a001eea795a52f0643704dd62d7e34cd3db33479
Pyae Sone Nyein <pyaesonenyein1996@gmail.com>
2024-03-18 14:04:47 +0630

    test

--- app/Models/OrderTranscation.php
+++ app/Models/OrderTranscation.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OrderTranscation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'subscription_id',
+        'grade_id',
+        'status'
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public static function boot() {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+    }
+
 }
