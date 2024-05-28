68ae9974f587f037406f11bfbea00f27c3d0a9a3
Pyae Sone Nyein <pyaesonenyein1996@gmail.com>
2024-05-28 11:02:31 +0630

    modified video and song fun

--- app/Http/Controllers/API/Subscribe/SubscriptionController.php
+++ app/Http/Controllers/API/Subscribe/SubscriptionController.php
@@ -100,11 +100,11 @@ class SubscriptionController extends Controller
         }
 
         // skip payment process
-        try {
-            return $this->getGradeAccess($student, $this->grade_id, $this->subscription_id);
-        } catch (\Throwable $th) {
-            return $th->getMessage();
-        }
+        // try {
+        //     return $this->getGradeAccess($student, $this->grade_id, $this->subscription_id);
+        // } catch (\Throwable $th) {
+        //     return $th->getMessage();
+        // }
 
         //ts
 
