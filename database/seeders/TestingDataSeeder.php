d16983d7387baf50c7f12e817ef08bc8704dd2b4
Pyae Sone Nyein <pyaesonenyein1996@gmail.com>
2024-06-13 14:07:08 +0630

    nth

--- database/seeders/TestingDataSeeder.php
+++ database/seeders/TestingDataSeeder.php
@@ -19,19 +19,19 @@ class TestingDataSeeder extends Seeder
         $userId = [1, 9];
 
         ///////////////// testing ////////////////
-        $done = [
-            ['count' => 1, 'claimed_3' => 0, 'claimed_5' => 0],
-            ['count' => 2, 'claimed_3' => 0, 'claimed_5' => 0],
-            ['count' => 3, 'claimed_3' => 0, 'claimed_5' => 0],
-            ['count' => 3, 'claimed_3' => 1, 'claimed_5' => 0],
-            ['count' => 4, 'claimed_3' => 0, 'claimed_5' => 0],
-            ['count' => 4, 'claimed_3' => 1, 'claimed_5' => 0],
-            ['count' => 5, 'claimed_3' => 0, 'claimed_5' => 0],
-            ['count' => 5, 'claimed_3' => 1, 'claimed_5' => 0],
-            ['count' => 5, 'claimed_3' => 0, 'claimed_5' => 1],
-            ['count' => 5, 'claimed_3' => 1, 'claimed_5' => 1],
+        // $done = [
+        //     ['count' => 1, 'claimed_3' => 0, 'claimed_5' => 0],
+        //     ['count' => 2, 'claimed_3' => 0, 'claimed_5' => 0],
+        //     ['count' => 3, 'claimed_3' => 0, 'claimed_5' => 0],
+        //     ['count' => 3, 'claimed_3' => 1, 'claimed_5' => 0],
+        //     ['count' => 4, 'claimed_3' => 0, 'claimed_5' => 0],
+        //     ['count' => 4, 'claimed_3' => 1, 'claimed_5' => 0],
+        //     ['count' => 5, 'claimed_3' => 0, 'claimed_5' => 0],
+        //     ['count' => 5, 'claimed_3' => 1, 'claimed_5' => 0],
+        //     ['count' => 5, 'claimed_3' => 0, 'claimed_5' => 1],
+        //     ['count' => 5, 'claimed_3' => 1, 'claimed_5' => 1],
 
-        ];
+        // ];
 
         // $lessons =  range(1, 10);
 
@@ -148,72 +148,72 @@ class TestingDataSeeder extends Seeder
 
 
         // Assessment Finished Data
-        DB::table('assessment_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'grade_id' => 1,
-                'assess_name' => 1,
-                'point' => 30,
-                'finish' => 1,
-            ]
-        ]);
-        DB::table('assessment_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'grade_id' => 1,
-                'assess_name' => 2,
-                'point' => 40,
-                'finish' => 1,
-            ]
-        ]);
-        DB::table('assessment_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'grade_id' => 1,
-                'assess_name' => 3,
-                'point' => 50,
-                'finish' => 1,
-            ]
-        ]);
-        DB::table('assessment_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'grade_id' => 1,
-                'assess_name' => 4,
-                'point' => 35,
-                'finish' => 1,
-            ]
-        ]);
         // DB::table('assessment_finish_data')->insert([
         //     [
         //         'student_id' => 9,
         //         'grade_id' => 1,
-        //         'assess_name' => 5,
+        //         'assess_name' => 1,
+        //         'point' => 30,
+        //         'finish' => 1,
+        //     ]
+        // ]);
+        // DB::table('assessment_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'grade_id' => 1,
+        //         'assess_name' => 2,
         //         'point' => 40,
         //         'finish' => 1,
         //     ]
         // ]);
+        // DB::table('assessment_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'grade_id' => 1,
+        //         'assess_name' => 3,
+        //         'point' => 50,
+        //         'finish' => 1,
+        //     ]
+        // ]);
+        // DB::table('assessment_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'grade_id' => 1,
+        //         'assess_name' => 4,
+        //         'point' => 35,
+        //         'finish' => 1,
+        //     ]
+        // ]);
+        // // DB::table('assessment_finish_data')->insert([
+        // //     [
+        // //         'student_id' => 9,
+        // //         'grade_id' => 1,
+        // //         'assess_name' => 5,
+        // //         'point' => 40,
+        // //         'finish' => 1,
+        // //     ]
+        // // ]);
 
-        // Assessment Finished Data for A Soe
-        DB::table('assessment_finish_data')->insert([
-            [
-                'student_id' => 3,
-                'grade_id' => 1,
-                'assess_name' => 1,
-                'point' => 30,
-                'finish' => 1,
-            ]
-        ]);
-        // Assessment Finished Data for A Soe
-        DB::table('assessment_finish_data')->insert([
-            [
-                'student_id' => 3,
-                'grade_id' => 1,
-                'assess_name' => 2,
-                'point' => 30,
-                'finish' => 1,
-            ]
-        ]);
+        // // Assessment Finished Data for A Soe
+        // DB::table('assessment_finish_data')->insert([
+        //     [
+        //         'student_id' => 3,
+        //         'grade_id' => 1,
+        //         'assess_name' => 1,
+        //         'point' => 30,
+        //         'finish' => 1,
+        //     ]
+        // ]);
+        // // Assessment Finished Data for A Soe
+        // DB::table('assessment_finish_data')->insert([
+        //     [
+        //         'student_id' => 3,
+        //         'grade_id' => 1,
+        //         'assess_name' => 2,
+        //         'point' => 30,
+        //         'finish' => 1,
+        //     ]
+        // ]);
 
 
         // assessmen5t each record
@@ -268,25 +268,25 @@ class TestingDataSeeder extends Seeder
 
         // // Login Bonus seeder
 
-        $now = Carbon::now();
-        $login_data = [
-            ['date' => Carbon::now()->addDays(2), 'day_count' => 3, 'claim' => 1],
-            ['date' => Carbon::now()->addDays(4), 'day_count' => 5, 'claim' => 0],
-            ['date' => Carbon::now()->addDays(6), 'day_count' => 7, 'claim' => 1],
-        ];
+        // $now = Carbon::now();
+        // $login_data = [
+        //     ['date' => Carbon::now()->addDays(2), 'day_count' => 3, 'claim' => 1],
+        //     ['date' => Carbon::now()->addDays(4), 'day_count' => 5, 'claim' => 0],
+        //     ['date' => Carbon::now()->addDays(6), 'day_count' => 7, 'claim' => 1],
+        // ];
 
 
-        foreach ($login_data as $k => $data) {
-            DB::table('student_login_bonuses')->insert([
-                [
-                    'student_id' => 9,
-                    'day_count' => $data['day_count'],
-                    'date' => $data['date'],
-                    'claim' => $data['claim'],
-                    'created_at' => Carbon::now(),
-                ]
-            ]);
-        }
+        // foreach ($login_data as $k => $data) {
+        //     DB::table('student_login_bonuses')->insert([
+        //         [
+        //             'student_id' => 9,
+        //             'day_count' => $data['day_count'],
+        //             'date' => $data['date'],
+        //             'claim' => $data['claim'],
+        //             'created_at' => Carbon::now(),
+        //         ]
+        //     ]);
+        // }
 
 
 
@@ -299,76 +299,76 @@ class TestingDataSeeder extends Seeder
 
 
         // assessmen5t each record
-        DB::table('assessment_each_record_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'assess_id' => 28,
-                'assess_name' => 5,
-                'grade_id' => 1,
-            ]
-        ]);
-        DB::table('assessment_each_record_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'assess_id' => 29,
-                'assess_name' => 5,
-                'grade_id' => 1,
-            ]
-        ]);
-        DB::table('assessment_each_record_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'assess_id' => 30,
-                'assess_name' => 5,
-                'grade_id' => 1,
-            ]
-        ]);
-        DB::table('assessment_each_record_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'assess_id' => 31,
-                'assess_name' => 5,
-                'grade_id' => 1,
-            ]
-        ]);
-        DB::table('assessment_each_record_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'assess_id' => 32,
-                'assess_name' => 5,
-                'grade_id' => 1,
-            ]
-        ]);
-        DB::table('assessment_each_record_finish_data')->insert([
-            [
-                'student_id' => 9,
-                'assess_id' => 33,
-                'assess_name' => 5,
-                'grade_id' => 1,
-            ]
-        ]);
+        // DB::table('assessment_each_record_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'assess_id' => 28,
+        //         'assess_name' => 5,
+        //         'grade_id' => 1,
+        //     ]
+        // ]);
+        // DB::table('assessment_each_record_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'assess_id' => 29,
+        //         'assess_name' => 5,
+        //         'grade_id' => 1,
+        //     ]
+        // ]);
+        // DB::table('assessment_each_record_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'assess_id' => 30,
+        //         'assess_name' => 5,
+        //         'grade_id' => 1,
+        //     ]
+        // ]);
+        // DB::table('assessment_each_record_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'assess_id' => 31,
+        //         'assess_name' => 5,
+        //         'grade_id' => 1,
+        //     ]
+        // ]);
+        // DB::table('assessment_each_record_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'assess_id' => 32,
+        //         'assess_name' => 5,
+        //         'grade_id' => 1,
+        //     ]
+        // ]);
+        // DB::table('assessment_each_record_finish_data')->insert([
+        //     [
+        //         'student_id' => 9,
+        //         'assess_id' => 33,
+        //         'assess_name' => 5,
+        //         'grade_id' => 1,
+        //     ]
+        // ]);
 
 
         // client testing user's lesson records -----------
-        $lessons =  range(1, 8);
+        // $lessons =  range(1, 8);
 
-        // foreach ([10,11,12,13] as  $id) {
-        foreach (range(1,9) as  $id) {
-            if($id != 3){
-                foreach ($lessons as $l) {
-                    DB::table('student_lessons')->insert([
-                        [
-                            'student_id' => $id,
-                            'grade_id' => 1,
-                            'lesson_id' => $l,
-                            // 'claimed_3' => $done[$k]['claimed_3'],
-                            // 'claimed_5' => $done[$k]['claimed_5'],
+        // // foreach ([10,11,12,13] as  $id) {
+        // foreach (range(1,9) as  $id) {
+        //     if($id != 3){
+        //         foreach ($lessons as $l) {
+        //             DB::table('student_lessons')->insert([
+        //                 [
+        //                     'student_id' => $id,
+        //                     'grade_id' => 1,
+        //                     'lesson_id' => $l,
+        //                     // 'claimed_3' => $done[$k]['claimed_3'],
+        //                     // 'claimed_5' => $done[$k]['claimed_5'],
 
-                        ]
-                    ]);
-                }
-            }
-        }
+        //                 ]
+        //             ]);
+        //         }
+        //     }
+        // }
 
 
 
